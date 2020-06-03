63f9b05dd6dd687796f99cc3e450c35a6ff5ec89
Stanislaw Hüll <huell@stud.uni-heidelberg.de>
2020-06-03 20:41:14 +0200

    stuff

--- c/main.c
+++ c/main.c
@@ -3,13 +3,31 @@
 
 int main(int argc, char** argv) {
 
-  char buffer_a[16];
-  char buffer_b[16];
+  int authorized = 0;
+  char sys_pass[16] = "secret!";
+  char usr_pass[16] = " preset";
 
-  strcpy(buffer_a, "123456789ABCDEF");
+  printf("enter password: \n");
+  // enter enough characters to overwrite sys_pass and usr_pass like so:
+  // strcpy(usr_pass, "1234567890abcdef" "1234567890abcdef" "1234567890abc" );
+  scanf("%s", usr_pass);
 
-  printf("buffer_a @ 0x%p: '%s'\n", (void*)buffer_a, buffer_a);
-  printf("buffer_b @ 0x%p: '%s'\n", (void*)buffer_b, buffer_b);
+  printf("usr_pass: %s\n", usr_pass);
+  printf("sys_pass: %s\n", sys_pass);
+  printf("authorized: %d\n", authorized);
+  printf("usr_pass   @ %p\n", (void*)usr_pass);
+  printf("sys_pass   @ %p\n", (void*)sys_pass);
+  printf("authorized @ %p\n", (void*)&authorized);
+
+  if (strcmp(sys_pass, usr_pass) == 0) {
+    authorized = 1;
+  }
+
+  if (authorized) {
+    printf("Authorized! Access granted.\n");
+  } else {
+    printf("Access denied.\n");
+  }
 
   return 0;
 }
