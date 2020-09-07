4f8067c0764e44b9457dc4091aa89fa9a4d4eb85
Stanislaw Hüll <st@nislaw.de>
2020-09-07 16:24:24 +0200

    update ipc template to use zsh-xi

--- c-ipc1/_setup.sh
+++ c-ipc1/_setup.sh
@@ -1,5 +1,15 @@
 # Linux shared memory IPC (C)
 
-st zsh -c 'nvim -p ./server.c client.c Makefile; zsh -i' &
-st zsh -c "echo  \"./server.c\nMakefile\" | entr -rcs \"make && ./server\"; zsh -i" &
-st zsh -c "echo  \"./client.c\nMakefile\" | entr -rcs \"make && ./client\"; zsh -i" &
+zsh-xi $TERMINAL <<EOF &
+nvim -p server.c client.c Makefile
+EOF
+
+zsh-xi $TERMINAL <<-EOF &
+echo "server.c\nMakefile" |
+  entr -rcs "make && ./server"
+EOF
+
+zsh-xi $TERMINAL <<EOF &
+echo "client.c\nMakefile" |
+  entr -rcs "sleep 0.1; make && ./client"
+EOF
